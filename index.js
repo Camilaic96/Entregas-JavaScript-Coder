dd50c32130b7dde8529bb3383da1497f9aee8de7
camila cuello <camilaicuello@hotmail.com>
2022-09-19 23:51:05 -0300

    Entregable 12-09 Clase 8 DOM

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-let opc
 let cantMujeres = 0
 let cantHombres = 0
 let cantNoBinarios = 0
@@ -6,18 +5,6 @@ const valorCuota = 1000
 let socios = []
 let nroSocio
 let sociosDeudores = []
-
-const listadoMenu = () => parseInt(prompt("Menú:\n1- Ingresar socios\n2- Cantidad de mujeres\n3- Cantidad de hombre\n4- Cantidad de personas no binarioas\n5- Promedio de edades\n6- Total recaudado en cuotas\n7- Mostrar lista de deudores\n8- Salir del programa"))
-let cantMuj = () => alert("Hay " + cantMujeres + " mujeres.")
-let cantHom = () => alert("Hay " + cantHombres + " hombres.")
-let cantNB = () => alert("Hay " + cantNoBinarios + " personas no binarias.")
-let promEdades = () => {
-    let totalEdades = socios.reduce( (acumulador, elemento) => acumulador + elemento.edad, 0)
-    alert("El promedio de edades es de " + Math.floor(totalEdades / socios.length) + " años.")
-}
-let recaudacionCuotas = () => {
-    alert("En total se recaudaron $" + valorCuota * (socios.length - sociosDeudores.length) + " en concepto de cuotas.")
-}
 
 class Socio {
     constructor(nroSocio, nombre, apellido, edad, genero, cuotaPaga) {
@@ -31,20 +18,20 @@ class Socio {
 }
 
 function agregarSocio() {
-    const ingValidGenero = function() {
+    const ingValidGenero = function () {
         let genero
-        do{
+        do {
             genero = prompt("Ingrese el genero del socio " + nroSocio + " (F, M ó NB)")
             genero = genero.toUpperCase()
-        }while(genero!=='M' && genero!=='F' && genero!=='NB')
+        } while (genero !== 'M' && genero !== 'F' && genero !== 'NB')
         return genero
     }
-    const ingValidCuota = function() {
+    const ingValidCuota = function () {
         let cuota
-        do{
-            cuota = prompt("¿Pagó la última cuotael socio " + nroSocio + "? (V ó F)");
+        do {
+            cuota = prompt("¿Pagó la última cuota el socio " + nroSocio + "? (V ó F)");
             cuota = cuota.toUpperCase()
-        }while(cuota!=='V' && cuota!=='F')
+        } while (cuota !== 'V' && cuota !== 'F')
         return cuota
     }
 
@@ -54,7 +41,7 @@ function agregarSocio() {
     let genero = ingValidGenero()
     if (genero == 'F') {
         cantMujeres += 1
-    } else if (genero == 'M'){
+    } else if (genero == 'M') {
         cantHombres += 1
     } else {
         cantNoBinarios += 1
@@ -63,7 +50,7 @@ function agregarSocio() {
     if (ingValidCuota() == 'V') {
         cuotaPaga = true
     }
-    
+
     let socioARegistrar = new Socio(
         nroSocio,
         nombre,
@@ -73,62 +60,59 @@ function agregarSocio() {
         cuotaPaga
     );
     socios.push(socioARegistrar);
-    if(cuotaPaga == false){
+    if (cuotaPaga == false) {
         sociosDeudores.push(socioARegistrar)
     }
     return socios;
 }
 
-function mostrarListaDeudores() {
-    sociosDeudores.forEach((elemento) => {
-        for (const propiedad in elemento) {
-            console.log(`${propiedad}: ${elemento[propiedad]}`);
-            }
-        })
-}
+function ingresarSocios() {
+    const ingNumSocio = () => parseInt(prompt("Ingrese el número de socio (0 para salir: )"))
 
-function tareasMenu(op) {
-    switch (op) {
-        case 1:
-            const ingNumSocio = () => parseInt(prompt("Ingrese el número de socio (0 para salir: )"))
-
-            nroSocio = ingNumSocio()
-            while (nroSocio != 0) {
-                socios = agregarSocio();
-                nroSocio = ingNumSocio()
-            }
-            break
-        case 2:
-            cantMuj()
-            break
-        case 3:
-            cantHom()
-            break
-        case 4:
-            cantNB()
-            break
-        case 5:
-            promEdades()
-            break
-        case 6:
-            recaudacionCuotas()
-            break
-        case 7:
-            mostrarListaDeudores()
-            break
-        case 8:
-            alert("Fin del programa")
-            break
+    nroSocio = ingNumSocio()
+    while (nroSocio != 0) {
+        socios = agregarSocio();
+        nroSocio = ingNumSocio()
     }
 }
 
+function mostrarListaDeudores() {
+    const contenedorDeudores = document.getElementById("contenedor-deudores")
+    contenedorDeudores.innerHTML = `<h3>Lista de deudores</h3>`
+    sociosDeudores.forEach((elemento) => {
+        for (const propiedad in elemento) {
+            console.log(`${propiedad}: ${elemento[propiedad]}`)
+        }
+        let column = document.createElement("div")
+        column.className = "col-md-12 mt-3"
+        column.id = `columna-${elemento.nroSocio}`
+        column.innerHTML = `
+            <div class="card">
+                <div class="card-body">
+                    <p class="card-text">Nº de socio: <b>${elemento.nroSocio}</b></p>
+                    <p class="card-text">Nombre: <b>${elemento.nombre}</b></p>
+                    <p class="card-text">Apellido: <b>${elemento.apellido}</b></p>
+                    <p class="card-text">Edad: <b>${elemento.edad}</b></p>
+                    <p class="card-text">Género: <b>${elemento.genero}</b></p>
+                </div>
+            </div>
+        `
+        contenedorDeudores.append(column)
+    })
+}
+
+function mostrarDatosSocios() {
+    let datosSocios = document.getElementById("datos-socios")
+    let totalEdades = socios.reduce((acumulador, elemento) => acumulador + elemento.edad, 0)
+    datosSocios.innerHTML = `<h3>Datos Socios</h3><p>Hay ${cantMujeres} mujeres, ${cantHombres} hombres y ${cantNoBinarios} personas no binarias</p><p>El promedio de edades es de ${Math.floor(totalEdades / socios.length)} años</p><p>En total se recaudaron $ ${valorCuota * (socios.length - sociosDeudores.length)} en concepto de cuotas.</p>`
+}
+
 function main() {
-    do {
-        do {
-            opc = listadoMenu()
-        } while (opc < 1 || opc > 8)
-        tareasMenu(opc)
-    } while (opc !== 8)
+    ingresarSocios()
+    const infoSocios = document.getElementById("titulo-info-socios")
+    infoSocios.innerHTML = `<h2>Información de los socios</h2>`
+    mostrarDatosSocios()
+    mostrarListaDeudores()
 }
 
 main();
